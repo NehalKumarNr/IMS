34312e15d60a0a079fa9b2e5822cc7dc27a0ca07
NehalKumarNr <nehalkumar14@gmail.com>
2024-09-16 10:20:29 -0400

    Doctype Created

---
+++ ims/inventory_management_system/doctype/ims_stock_entry/ims_stock_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Nehal Kumar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('IMS Stock Entry', {
+	// refresh: function(frm) {
+
+	// }
+});
