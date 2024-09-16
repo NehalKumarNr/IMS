7c3e6d664dfdd2073edfcdf4f86d3e95a17d51e1
ubuntu <ubuntu@ubuntu-2204>
2024-09-16 08:40:25 -0400

    feat: Initialize App

---
+++ ims/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Inventory Management System",
+			"type": "module",
+			"label": _("Inventory Management System")
+		}
+	]
