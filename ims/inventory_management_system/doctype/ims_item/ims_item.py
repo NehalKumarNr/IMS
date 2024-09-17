1b3fba050d073db0395fad00c9b6b0f987b3e616
NehalKumarNr <nehalkumar14@gmail.com>
2024-09-17 03:18:56 -0400

    creating stock entry through item

--- ims/inventory_management_system/doctype/ims_item/ims_item.py
+++ ims/inventory_management_system/doctype/ims_item/ims_item.py
@@ -4,6 +4,18 @@
 import frappe
 from frappe.model.document import Document
 from frappe.model.naming import make_autoname
+from frappe.utils import (
+    cint,
+    cstr,
+    flt,
+    formatdate,
+    get_link_to_form,
+    getdate,
+    now_datetime,
+    nowtime,
+    strip,
+    strip_html,
+)
 
 class IMSItem(Document):
     def autoname(self):
@@ -13,6 +25,8 @@ class IMSItem(Document):
     def after_insert(self):
         if self.standard_rate:
             self.item_price_list_created()
+        if self.opening_stock:
+            self.create_opening_stock()
 
     def item_price_list_created(self):
         price_list = frappe.db.get_single_value('IMS Setting', 'price_list')
@@ -29,3 +43,32 @@ class IMSItem(Document):
             item_price.insert()
         else:
             frappe.throw("Select Price list in IMS setting")
+
+    def create_opening_stock(self):
+        if not self.maintain_stock:
+            return
+
+        if not self.valuation_rate and not self.standard_rate:
+            frappe.throw(_("Valuation Rate or Standard Rate is mandatory if Opening Stock entered"))
+
+        warehouse = frappe.db.get_single_value('IMS Setting', 'default_warehouse')
+        if warehouse:
+            stock_entry = frappe.get_doc({
+                "doctype": "IMS Stock Entry",
+                "stock_entry_type": "Material Issue",
+                "posting_date": getdate(),
+                "posting_time": nowtime(),
+                "items": [
+                    {
+                        "target_warehouse": warehouse,
+                        "item_code": self.name,
+                        "qty": self.opening_stock,
+                        "rate": self.valuation_rate or self.standard_rate
+                    }
+                ]
+            })
+            stock_entry.insert()
+            stock_entry.submit()
+        else:
+            frappe.throw("Select Warehouse in IMS setting")
+    
