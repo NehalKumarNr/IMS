7ca1ac40085c4d8687eea4fe190d59e6e461381b
NehalKumarNr <nehalkumar14@gmail.com>
2024-09-17 06:28:17 -0400

    tree view for warehouse

--- ims/inventory_management_system/doctype/ims_warehouse/ims_warehouse.js
+++ ims/inventory_management_system/doctype/ims_warehouse/ims_warehouse.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('IMS Warehouse', {
-	// refresh: function(frm) {
-
-	// }
+	setup(frm) {
+		frm.set_query("parent_ims_warehouse", function() {
+			return {
+				filters: {
+					"is_group": 1
+				}
+			};
+		});
+	}
 });
